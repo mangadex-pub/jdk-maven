16c1b714fc32f95cbc4d83b0a96a046b61068286
Tristan <tristan@mangadex.org>
2023-02-18 20:55:32 +0000

    Update to Maven 3.9.0

--- install-maven.sh
+++ install-maven.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-export MAVEN_VERSION="3.8.7"
-export MAVEN_TARBALL_SHA="21c2be0a180a326353e8f6d12289f74bc7cd53080305f05358936f3a1b6dd4d91203f4cc799e81761cf5c53c5bbe9dcc13bdb27ec8f57ecf21b2f9ceec3c8d27"
+export MAVEN_VERSION="3.9.0"
+export MAVEN_TARBALL_SHA="1ea149f4e48bc7b34d554aef86f948eca7df4e7874e30caf449f3708e4f8487c71a5e5c072a05f17c60406176ebeeaf56b5f895090c7346f8238e2da06cf6ecd"
 export MAVEN_TARBALL_BASEURL="https://dlcdn.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries"
 
 MAVEN_INSTALL_DIR="${1:-"/usr/share/maven"}"
